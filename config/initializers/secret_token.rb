ee85e642ca0d086b838f9ed388da058dcb9bc568
Kieran Klaassen <kieranklaassen@gmail.com>
2012-12-20 12:54:05 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MercuryWindowBug::Application.config.secret_token = '907a1559f2c52271baba01d1c57a84e23284446b44e959b071a241378c4084d43c05f4335fd88f34f7eb78f067ed9645ec63fe036ef0262b44ed1d6a6d329aeb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MercuryWindowBug::Application.config.session_store :cookie_store, key: '_mercury_window_bug_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MercuryWindowBug::Application.config.session_store :active_record_store
